56bb556a336e13beac414e3efd1051f6da56ca90
Paweł Buglewicz <illmoded@gmail.com>
2015-06-10 20:56:51 +0200

    głupie skaczące króliki

--- gra.py
+++ gra.py
@@ -11,48 +11,129 @@ d = teren.Dzunkla()
 rozmiar = 100
 ziemia = [[0 for x in range(rozmiar)] for y in range(rozmiar)]
 
-#nie wiem po co takie cos, ale jest...
+# nie wiem po co takie cos, ale jest...
 for x in range(rozmiar):
     for y in range(rozmiar):
         if 60 > x > 40 and 60 > y > 40:
-            ziemia[x][y]=(copy.deepcopy(d))
-            ziemia[x][y].pozycja = x,y
+            ziemia[x][y] = (copy.deepcopy(d))
+            ziemia[x][y].pozycja = x, y
         else:
-            ziemia[x][y]=(copy.deepcopy(p))
-            ziemia[x][y].pozycja = x,y
-        # print(ziemia[x][y].pozycja)
+            ziemia[x][y] = (copy.deepcopy(p))
+            ziemia[x][y].pozycja = x, y
+            # print(ziemia[x][y].pozycja)
 
 listazwieraat = []
 zwierz = zwierza.Zwierz()
 
-#tu jeszcze while ogolny
-while len(listazwieraat)<100: #generuje
+# tu jeszcze while ogolny
+while len(listazwieraat) < 100:  # generuje
     listazwieraat.append(zwierz)
 
-for zwierze in listazwieraat: #modyfikuje
+for zwierze in listazwieraat:  # modyfikuje
 
-    zwierze.x = r.randint(0,rozmiar)
-    zwierze.y = r.randint(0,rozmiar)
+    zwierze.x = r.randint(0, rozmiar)
+    zwierze.y = r.randint(0, rozmiar)
 
-    zwierze.id = zwierze  #adres w pamieci, powinien byc unikatowy!!!!!!!!!!!!!
+    zwierze.id = zwierze  # adres w pamieci, powinien byc unikatowy!!!!!!!!!!!!!
 
     for i in xrange(8):
-        zwierze.genom.append(r.randint(-10,10))
+        zwierze.genom.append(r.randint(-10, 10))
 
-
+# albo stworzyc konstruktor i gnerowac w taki sposob, w sumie bardziej pythonowe by bylo
 
 # tak wyglada wzrost drzew
-los=0
+los = 0
 for x in range(rozmiar):
     for y in range(rozmiar):
 
-        los = r.randint(0,100)
+        los = r.randint(0, 100)
 
         if ziemia[x][y].szansa_na_wzrost > los:
             ziemia[x][y].rosnie_drzewo()
 
-        # print(los, ziemia[x][y].szansa_na_wzrost,ziemia[x][y].szansa_na_wzrost>los,ziemia[x][y].energia)
+            # print(los, ziemia[x][y].szansa_na_wzrost,ziemia[x][y].szansa_na_wzrost>los,ziemia[x][y].energia)
 
 # ruch zwierzat
+for krolik in listazwieraat:
+    # to jest tak glupie ze nie moge. jak to zrobic inaczej?
 
+    wrrr = r.randint(0, 8)
+    if wrrr == 1:
+        if 0 > krolik.x - krolik.genom[wrrr] > rozmiar:
+            krolik.x -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x -= krolik.genom[wrrr]
+        if 0 > krolik.y + krolik.genom[wrrr] > rozmiar:
+            krolik.y += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y += krolik.genom[wrrr]
 
+    if wrrr == 2:
+        if 0 > krolik.x - 0 > rozmiar:
+            krolik.x -= 0 % rozmiar
+        else:
+            krolik.x -= 0
+        if 0 > krolik.y + krolik.genom[wrrr] > rozmiar:
+            krolik.y += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y += krolik.genom[wrrr]
+
+    if wrrr == 3:
+        if 0 > krolik.x + krolik.genom[wrrr] > rozmiar:
+            krolik.x += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x += krolik.genom[wrrr]
+        if 0 > krolik.y + krolik.genom[wrrr] > rozmiar:
+            krolik.y += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y += krolik.genom[wrrr]
+
+    if wrrr == 4:
+        if 0 > krolik.x + krolik.genom[wrrr] > rozmiar:
+            krolik.x += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x += krolik.genom[wrrr]
+        if 0 > krolik.y + 0 > rozmiar:
+            krolik.y += 0 % rozmiar
+        else:
+            krolik.y += 0
+
+    if wrrr == 5:
+        if 0 > krolik.x + krolik.genom[wrrr] > rozmiar:
+            krolik.x += krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x += krolik.genom[wrrr]
+        if 0 > krolik.y - krolik.genom[wrrr] > rozmiar:
+            krolik.y -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y -= krolik.genom[wrrr]
+
+    if wrrr == 6:
+        if 0 > krolik.x - 0 > rozmiar:
+            krolik.x -= 0 % rozmiar
+        else:
+            krolik.x -= 0
+        if 0 > krolik.y - krolik.genom[wrrr] > rozmiar:
+            krolik.y -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y -= krolik.genom[wrrr]
+
+    if wrrr == 7:
+        if 0 > krolik.x - krolik.genom[wrrr] > rozmiar:
+            krolik.x -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x -= krolik.genom[wrrr]
+        if 0 > krolik.y - krolik.genom[wrrr] > rozmiar:
+            krolik.y -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.y -= krolik.genom[wrrr]
+
+    if wrrr == 8:
+        if 0 > krolik.x - krolik.genom[wrrr] > rozmiar:
+            krolik.x -= krolik.genom[wrrr] % rozmiar
+        else:
+            krolik.x -= krolik.genom[wrrr]
+        if 0 > krolik.y + 0 > rozmiar:
+            krolik.y += 0 % rozmiar
+        else:
+            krolik.y += 0
